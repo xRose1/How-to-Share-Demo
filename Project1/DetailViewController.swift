6e5f811a14c1066171edda45345cc5c59d818cdc
Sanjeev Bedasee <ibedasee@gmail.com>
2019-06-14 17:10:41 -0400

    Final

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Sbedx4 on 04/14/2019.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+	@IBOutlet var imageView: UIImageView!
+	var selectedImage: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+
+		if let imageToLoad = selectedImage {
+			imageView.image  = UIImage(named: imageToLoad)
+		}
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+	@objc func shareTapped() {
+		let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+		vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+		present(vc, animated: true)
+	}
+}
